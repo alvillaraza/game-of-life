e4b7ba3d2ddc2013a67eefa6594f36683280b666
alexis villaraza <alexis.borja@gmail.com>
2020-07-29 13:01:49 -0700

    Updates

---
+++ precache-manifest.e35e5665cb503cf74018872fd70a979e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39483212936108fa2fb814d1fbd729c",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "e108a8a95324b3d7bf28",
+    "url": "/game-of-life/static/css/main.7a2fc740.chunk.css"
+  },
+  {
+    "revision": "ca4d08c8c30fc96717a7",
+    "url": "/game-of-life/static/js/2.038d2dae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.038d2dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e108a8a95324b3d7bf28",
+    "url": "/game-of-life/static/js/main.b16f5dc7.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
